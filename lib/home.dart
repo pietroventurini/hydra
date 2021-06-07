3bbe54c747b382b168baefa82afea749979c5f4e
pietroventurini <pietro97venturini@gmail.com>
2021-06-07 16:55:34 +0200

    Checkpoint

--- lib/home.dart
+++ lib/home.dart
@@ -5,22 +5,172 @@ class HomeTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
+    var cardDecoration = BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.all(Radius.circular(24)),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.grey.withOpacity(.23),
+          blurRadius: 30,
+          offset: Offset(0, 10),
+        )
+      ],
+    );
+
+    var progressCard = Container(
+      height: 180,
+      margin: EdgeInsets.only(top: 24, left: 24, right: 24),
+      decoration: cardDecoration,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Container(
-            height: 200,
-            margin: EdgeInsets.only(top: 20),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.all(Radius.circular(24)),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 28),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    "Today's progress",
+                    style: TextStyle(
+                      fontFamily: 'Avenir',
+                      fontSize: 16,
+                      color: Colors.black54,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  RichText(
+                    text: TextSpan(
+                      style: TextStyle(
+                        fontFamily: 'Avenir',
+                        fontSize: 18,
+                        color: Colors.black54,
+                        fontWeight: FontWeight.w400,
+                      ),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: "1200",
+                          style: TextStyle(
+                            fontSize: 28,
+                            color: Colors.black87,
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+                        TextSpan(
+                          text: "/2000ml",
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-          Container(
-
+          Expanded(
+            child: Center(
+              child: SizedBox(
+                width: 100,
+                height: 100,
+                child: CircularProgressIndicator(
+                  value: 0.3,
+                  strokeWidth: 10.0,
+                  backgroundColor: Colors.black12,
+                ),
+              ),
+            ),
           ),
         ],
-      )
+      ),
+    );
+
+    var newRecordTab = Container(
+      margin: EdgeInsets.only(top: 20, left: 26, right: 26),
+      decoration: cardDecoration,
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Row(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Drink check-in",
+                  style: TextStyle(
+                    fontFamily: 'Avenir',
+                    fontSize: 20,
+                    color: Colors.black87,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Row(
+                  children: [
+                    SizedBox(
+                      height: 50,
+                      width: 100,
+                      child: TextFormField(
+                        initialValue: "20:37",
+                        decoration: InputDecoration(
+                          icon: Icon(Icons.schedule_rounded),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                color: Colors.red,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+
+    var historyTab = Expanded(
+      child: Container(
+        //color: Colors.white,
+        margin: EdgeInsets.only(left: 26, top: 30),
+        child: ListView.builder(
+          reverse: true,
+          itemCount: 15,
+          itemBuilder: (BuildContext context, index) {
+            return Container(
+              margin: EdgeInsets.only(bottom: 20),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: Text("Time ${index + 1}"),
+                  ),
+                  Expanded(
+                    flex: 8,
+                    child: Container(
+                      color: Colors.white,
+                      height: 60,
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+
+    return SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          progressCard,
+          //newRecordTab,
+          historyTab,
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -14,17 +14,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: const Color(0xfff3f7fb)
-      ),
+          primarySwatch: Colors.blue,
+          scaffoldBackgroundColor: const Color(0xfff3f7fb)),
       restorationScopeId: 'root',
-      home: MainPage(title: 'Flutter Demo Home Page', restorationId: 'bottom_navigation_labels_demo'),
+      home: MainPage(
+          title: 'Flutter Demo Home Page',
+          restorationId: 'bottom_navigation_labels_demo'),
     );
   }
 }
 
 class MainPage extends StatefulWidget {
-  MainPage({Key key, this.title, @required this.restorationId}) : super(key: key);
+  MainPage({Key key, this.title, @required this.restorationId})
+      : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -78,18 +80,14 @@ class _MainPageState extends State<MainPage> with RestorationMixin {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    
+
     final icons = List<IconData>.unmodifiable([
-      Icons.home,
+      Icons.home_rounded,
       Icons.show_chart_rounded,
-      Icons.account_circle_rounded
+      Icons.account_circle_rounded,
     ]);
 
-    final labels = List<String>.unmodifiable([
-      "Home",
-      "Stats",
-      "Account"
-    ]);
+    final labels = List<String>.unmodifiable(["Home", "Stats", "Account"]);
 
     final _screens = List<Widget>.unmodifiable([
       HomeTab(),
@@ -98,24 +96,55 @@ class _MainPageState extends State<MainPage> with RestorationMixin {
     ]);
 
     return Scaffold(
-      body: _screens[_navIndex.value],
-      bottomNavigationBar: MobileNavBar(
+        body: _screens[_navIndex.value],
+        bottomNavigationBar: MobileNavBar(
           activeIndex: _navIndex.value,
           icons: icons,
           labels: labels,
           onTap: _onItemTapped,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => _openNewRecordMenu(),
+          tooltip: "New check-in",
+          child: const Icon(Icons.add),
+        ),
+        floatingActionButtonLocation:
+            FloatingActionButtonLocation.centerDocked);
+  }
+
+  void _openNewRecordMenu() {
+    showModalBottomSheet(
+      context: context,
+      backgroundColor: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(24.0),
+          topRight: Radius.circular(24.0),
+        ),
       ),
+      isScrollControlled: true,
+      builder: (BuildContext context) {
+        return Container(
+          height: MediaQuery.of(context).copyWith().size.height * 0.85,
+          child: Center(
+            child: Column(
+              children: [],
+            ),
+          ),
+        );
+      },
     );
   }
 }
 
-class MobileNavBar extends  StatelessWidget {
-  MobileNavBar({
-    @required this.activeIndex,
-    @required this.icons,
-    @required this.labels,
-    @required this.onTap
-  }) : assert(activeIndex < icons.length), assert(icons.length == labels.length);
+class MobileNavBar extends StatelessWidget {
+  MobileNavBar(
+      {@required this.activeIndex,
+      @required this.icons,
+      @required this.labels,
+      @required this.onTap})
+      : assert(activeIndex < icons.length),
+        assert(icons.length == labels.length);
 
   final int activeIndex;
   final List<IconData> icons;
@@ -124,16 +153,11 @@ class MobileNavBar extends  StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-
     return BottomNavigationBar(
       showUnselectedLabels: false,
       items: <BottomNavigationBarItem>[
-        for (var i=0; i < icons.length; i++)
-          BottomNavigationBarItem(
-            icon: Icon(icons[i]),
-            label: labels[i]
-          )
+        for (var i = 0; i < icons.length; i++)
+          BottomNavigationBarItem(icon: Icon(icons[i]), label: labels[i])
       ],
       currentIndex: activeIndex,
       onTap: onTap,
